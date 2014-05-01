52bdc24ed7a23c73845d6e822f590111cb5a49ab
Yossof Eshaq <yossofeshaq@gmail.com>
2014-05-01 15:45:44 +0100

    fixed closure bug

--- bkgSyst.py
+++ bkgSyst.py
@@ -34,12 +34,16 @@ def translate(den=None, num=None, sample="", considerLumi=False, afterTrigger=Fa
             outErr.append(None)
         else:
             outErr.append(abs(o)*math.sqrt((nE/n)**2 + (dE/d)**2))
+            #outErr.append(math.sqrt((nE/d)**2 + (dE*n/(d**2))**2))
     return out, outErr
 
 def observed(obs=None, sample=""):
     dataSample = "n%s"% sample.capitalize()
     out  =  obs._observations[dataSample]
     outErr = [math.sqrt(x) for x in out]
+    if len(out)>8:
+        out=(None,None)+out[2:]
+        outErr=(None,None)+tuple(outErr[2:])
     return out, outErr
 
 
@@ -47,10 +51,12 @@ def predicted(fromCat=None, toCat=None, sample=""):
     tf, tfe = translate(den=fromCat,
                         num=toCat,
                         sample=sample)
-
     dataSample = "n%s"% sample.capitalize()
     val = fromCat._observations[dataSample]
     valErr = [math.sqrt(x) for x in val]
+    if len(val)>8:
+        val=(None,None)+val[2:]
+        valErr=(None,None)+tuple(valErr[2:])
     out = []
     outErr = []
     for t,x in zip(tf,val):
@@ -58,18 +64,20 @@ def predicted(fromCat=None, toCat=None, sample=""):
             out.append(None)
             continue
         out.append(t*x)
-
     for n, d, nE, dE, o in zip(tf, val, tfe, valErr, out):
         if (None in [n, d, nE, dE, o]) or (not n) or (not d):
             outErr.append(None)
         else:
             outErr.append(abs(o)*math.sqrt((nE/n)**2 + (dE/d)**2))
+            # Below is Goldmann's exact variance for products (it gives the same answer as above :)
+            #outErr.append(math.sqrt((n*dE)**2 + (d*nE)**2 + (nE*dE)**2))
     return out, outErr
+
 
 def closure(obs, obsE, pre, preE):
     out = []
-    for x,y in zip(pre, obs):
-        if x == None:
+    for x,y in zip(obs,pre):
+        if x == None or x == 0.0:
             out.append(None)
             continue
         if y == 0.0:
@@ -78,23 +86,25 @@ def closure(obs, obsE, pre, preE):
         out.append((x-y)/y)
 
     outErr = []
-    for n, d, nE, dE, o in zip(obs, pre, obsE, preE, out):
-        if (None in [n, d, nE, dE, o]) or (not n) or (not d):
+    for o, p, oE, pE, o in zip(obs, pre, obsE, preE, out):
+        if (None in [o, p, oE, pE, o]) or (not o) or (not p):
             outErr.append(None)
         else:
-            outErr.append(abs(o)*math.sqrt((nE/n)**2 + (dE/d)**2))
+            #outErr.append(abs(o)*math.sqrt((oE/o)**2 + (pE/p)**2))
+            outErr.append(math.sqrt((oE/p)**2 + (pE*o/(p**2))**2))
     return out, outErr
     
 def oneDataset(canvas = None, legend=None, val=None, valErr=None,
                data = None, name = "", iTest = 0, color = None,
                afterTrigger = False, markerStyle=None, label=None) :
     htMeans = data.htMeans()
+    htLow = data.htBinLowerEdges()
     graphs = []
     canvas.cd(0)
     r.gPad.SetTickx()
     r.gPad.SetTicky()
     
-    assert len(set([len(htMeans), len(val), len(valErr)]))==1
+    assert len(set([len(htMeans), len(val), len(valErr), len(htLow)]))==1
     
     gr = r.TGraphErrors()
     gr.SetName("%s"%(name))
@@ -103,7 +113,7 @@ def oneDataset(canvas = None, legend=None, val=None, valErr=None,
     gr.SetTitle(";<H_{T}> in bin (GeV); ( N_{obs} - N_{pred} ) / N_{pred}")
 
     iGraph = 0
-    for h,t,tE in zip(htMeans, val, valErr) :
+    for h,t,tE in zip(htLow, val, valErr) :
         if t is None : continue
         shift = (iTest*5.5)*(1 if iTest % 2 == 0 else -1)
         gr.SetPoint(iGraph, h+(shift), t)
@@ -112,15 +122,15 @@ def oneDataset(canvas = None, legend=None, val=None, valErr=None,
     gr.Draw("psame" if iTest else "ap")
     print "\n\n\n\n", gr.GetName()
     func = r.TF1("func","pol0",200,1300)
-    gr.Fit("func", "FR")
+    gr.Fit("func", "R")
     legend.AddEntry(gr, label, "p")
     legend.Draw()
     hist = gr.GetHistogram()
     for axis in [hist.GetXaxis(), hist.GetYaxis()] :
         axis.SetTitleSize(1.4*axis.GetTitleSize())
         axis.SetTitleOffset(axis.GetTitleOffset()*.9)
-    hist.SetMinimum(-2.)
-    hist.SetMaximum(4)
+    hist.SetMinimum(-1.)
+    hist.SetMaximum(2.4)
     graphs.append(gr)
     return graphs
 
@@ -131,33 +141,32 @@ def plot(dataset = {}, tag = "", factors = ["gZ", "muW", "mumuZ"][:2]) :
     misc = []
     slices = dataset["slices"] #assume first list of slices contains the subsequent ones
     module = dataset["module"]
-
-    tests  = {"alphaT":{"num": getattr(module,"data_%s"%slices[-2:-1][0])(),
-                        "den": getattr(module,"data_%s"%slices[-1:][0])(),
+    tests  = {"alphaT":{"num": getattr(module,"data_%s"%slices["ge55"])(),
+                        "den": getattr(module,"data_%s"%slices["l55"])(),
                         "sample": "muon",
                         "markerStyle":24,
                         "label": "#alpha_{T} < 0.55 #rightarrow #alpha_{T} > 0.55 (#mu + jets)"},
 
-              "zeroBtoOneB":{"num": getattr(module,"data_%s"%slices[1:2][0])(),
-                             "den": getattr(module,"data_%s"%slices[:1][0])(),
+              "zeroBtoOneB":{"num": getattr(module,"data_%s"%slices["1b"])(),
+                             "den": getattr(module,"data_%s"%slices["0b"])(),
                              "sample": "muon",
                              "markerStyle":25,
                              "label": "0 b-tags #rightarrow 1 b-tags (#mu + jets)"},
 
-              "oneBtoTwoB":{"num": getattr(module,"data_%s"%slices[2:3][0])(),
-                            "den": getattr(module,"data_%s"%slices[1:2][0])(),
+              "oneBtoTwoB":{"num": getattr(module,"data_%s"%slices["2b"])(),
+                            "den": getattr(module,"data_%s"%slices["1b"])(),
                             "sample": "muon",
                             "markerStyle":26,
                             "label": "1 b-tags #rightarrow 2 b-tags (#mu + jets)"},
               
-              "jBinOneTojBinTwoMu":{"num": getattr(module,"data_%s"%slices[-4:-2][0])(),
-                                    "den": getattr(module,"data_%s"%slices[-3:-2][0])(),
+              "jBinOneTojBinTwoMu":{"num": getattr(module,"data_%s"%slices["ge4j"])(),
+                                    "den": getattr(module,"data_%s"%slices["le3j"])(),
                                     "sample": "muon",
                                     "markerStyle":32,
                                     "label": "2 #leq N_{jet} #leq 3 #rightarrow N_{jet} #geq 4 (#mu + jets)"},
               
-              "jBinOneTojBinTwoPhot":{"num": getattr(module,"data_%s"%slices[-4:-2][0])(),
-                                      "den": getattr(module,"data_%s"%slices[-3:-2][0])(),
+              "jBinOneTojBinTwoPhot":{"num": getattr(module,"data_%s"%slices["ge4j"])(),
+                                      "den": getattr(module,"data_%s"%slices["le3j"])(),
                                       "sample": "phot",
                                       "markerStyle":27,
                                       "label": "2 #leq N_{jet} #leq 3 #rightarrow N_{jet} #geq 4 (#mu + jets)"},
@@ -206,12 +215,13 @@ if d=="2012pf" :
 
     for i,j in enumerate(["ge4j", "le3j"]) :
         bs = ["0b", "1b", "2b", "3b"]
-        slices = ["%s_%s"%(b,j) for b in bs]
-        slices += ["ge4j", "le3j"]
-        slices += ["%s_%s"%(aT,j) for aT in ["ge55","l55"]]
-        datasets = [{"name": "pf", "module": take1, "slices": slices, "color":color1[j]},
-                    {"name": "calo", "module": take1_calo, "slices": slices, "color":color1[j]}
-                     ]
+        slices = zip(bs,["%s_%s"%(b,j) for b in bs])
+        slices += zip(["ge4j", "le3j"],["ge4j", "le3j"])
+        slices += zip(["ge55","l55"],["%s_%s"%(aT,j) for aT in ["ge55","l55"]])
+        datasets = [{"name": "pf", "module": take1, "slices": dict(slices), "color":color1[j]},
+                    {"name": "calo", "module": take1_calo, "slices": dict(slices), "color":color1[j]}
+                    ]
+        
         for dct in datasets:
             plot(dct, tag = j)
         
